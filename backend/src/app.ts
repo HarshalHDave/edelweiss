29c2ed9695c4dabe89d7ffddafcff26350137a04
DS <mailforgautamvg@gmail.com>
2023-07-01 23:34:29 +0530

    moved interfaces out of global and back into app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,43 @@ import logger from './lib/logger'
 
 const months = 'JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC'.split(', ')
 
+interface Data {
+	stocks: Array<Stock>
+	spot_stocks: Array<SpotStock>
+}
+
+interface Stock {
+	name: string
+	options: Array<Option>
+}
+
+interface SpotStock {
+	name: string
+	data: Array<TimeVariantData>
+}
+
+interface Option {
+	trading_symbol: string
+	type: 'cal' | 'put' | 'fut'
+	expiry_date: number // timestamp in milliseconds (append 3:30 evening)
+	strike: number | null // will be null for type='fut'
+	data: Array<TimeVariantData>
+}
+
+interface TimeVariantData {
+	timestamp: Number
+	ltq: number
+	ltp: number
+	vol: number
+	bid: number
+	ask: number
+	bid_qty: number
+	ask_qty: number
+	oi: number
+	prev_oi: number
+	prev_close_price: number
+}
+
 class App extends EventEmitter {
 	#socket: Socket
 	stocks: Array<Stock> = []
