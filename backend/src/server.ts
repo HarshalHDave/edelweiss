19cea46b04cd83179e0f5ee520c9d0ae86e71b25
DS <mailforgautamvg@gmail.com>
2023-07-03 01:15:17 +0530

    restructured data

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import type { Server as HttpServer } from 'http'
 import App from './app'
 
 import './types'
+import logger from './lib/logger'
 
 class Server extends SocketServer {
 	constructor(http_server: HttpServer, app: App) {
@@ -20,13 +21,25 @@ class Server extends SocketServer {
 			// Fetch current state of app and send to client
 			// socket.emit('stocks', app.stocks)
 			// socket.emit('spot_stocks', app.spot_stocks)
-			socket.emit('realtime_options', app.realtime_options)
+			// socket.emit('realtime_options', app.realtime_options)
 			// Assign event listener to listen to the state of app
 			// Upon each new event, send state of the app to client
-			app.on('update', () => {
-				socket.emit('realtime_options', app.realtime_options)
-				// socket.emit('stocks', app.stocks)
-				// socket.emit('spot_stocks', app.spot_stocks)
+			// app.on('update', () => {
+			// socket.emit('realtime_options', app.realtime_options)
+			// socket.emit('stocks', app.stocks)
+			// socket.emit('spot_stocks', app.spot_stocks)
+			// })
+
+			socket.on('req', (token: string, view: View, opts: ViewOptions) => {
+				logger.debug('Recevied req', { token, view, opts })
+				app.req_view(view, opts, (data: any) => {
+					logger.debug('Recevied res')
+					const res: ResOptions = {
+						token,
+						data
+					}
+					socket.emit('res', res)
+				})
 			})
 		})
 	}
