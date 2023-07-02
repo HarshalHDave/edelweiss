f0a6ed75b298429c6ee26777e12c3402e738b210
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-07-02 23:01:02 +0530

    feat: add charts in frontend

--- backend/src/db/repositories/option.ts
+++
@@ -1,5 +0,0 @@
-import data_source from '../data_source'
-import Option from '../entities/option'
-
-const repo = data_source.getRepository(Option)
-export default repo
--- frontend/src/lib/config.ts
+++
@@ -1,3 +0,0 @@
-// host and port of our backend
-export const host = 'localhost'
-export const port = 80
