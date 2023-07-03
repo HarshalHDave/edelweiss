f92e86cfbd4881e05a6a18a06a25ca90a34a6c52
DS <mailforgautamvg@gmail.com>
2023-07-03 21:09:40 +0530

    fixing undefined error

--- frontend/src/lib/Context.tsx
+++ frontend/src/lib/Context.tsx
@@ -4,41 +4,59 @@ import { io } from "socket.io-client";
 // Config
 import { host, port } from "./config";
 
-// Data type stored in this context
-// Data will be initially null
-export type Data = Array<Company> | null;
-
 // We use this token to uniquely identify each request
-const token = Math.random() * 3711;
+const my_token = `${Math.random() * 3711}`;
 
 // The view that is passed to the server
 const view: View = "home";
 
 // The view options passed to the server
 const view_options: ViewOptions = {
-  type: "historical",
+	type: "historical",
 };
 
 // The context
-const ctx = React.createContext<Data>(null);
+const ctx = React.createContext<Company[]>([]);
 export default ctx;
+
+let call_len = -1;
+let put_len = -1;
 
 // The provider
 export function StocksProvider(props: PropsWithChildren) {
-  // Data is initially null
-  const [data, setData] = useState<Data>(null);
+	// Data is initially null
+	const [data, setData] = useState<Company[]>([]);
 
-  // When the component mounts
-  useEffect(() => {
-    // Connect to the server
-    const socket = io(`${host}:${port}`);
-    socket.on("res", (token: string, data: Data) => {
-      if (token == token) setData(data);
-    });
-    socket.emit("req", token, view, view_options);
-  }, []);
+	// When the component mounts
+	useEffect(() => {
+		// Connect to the server
+		const socket = io(`${host}:${port}`);
+		socket.on("res", (token: string, data: Company[]) => {
+			const target_cmp = data.find((d) => d.name == "MAINIDX");
+			if (target_cmp) {
+				const target_opt = target_cmp.options.find(
+					(o) => o.id == "MAINIDX27JUL2317000"
+				);
+				if (target_opt) {
+					if (target_opt.call.length != call_len) {
+						call_len += target_opt.call.length;
+						console.log("Length of call: ", target_opt.call.length);
+						console.log("Call: ", target_opt.call);
+					}
+					if (target_opt.call.length != put_len) {
+						put_len += target_opt.put.length;
+						console.log("Length of put: ", target_opt.put.length);
+						console.log("Put: ", target_opt.put);
+					}
+				}
+			}
 
-  return <ctx.Provider value={data}>{props.children}</ctx.Provider>;
+			if (token == my_token) setData(data);
+		});
+		socket.emit("req", my_token, view, view_options);
+	}, []);
+
+	return <ctx.Provider value={data}>{props.children}</ctx.Provider>;
 }
 
 // The consumer
