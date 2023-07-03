81e87b6b2474740c6ae62ec3278d2f866b58d35c
DS <mailforgautamvg@gmail.com>
2023-07-04 03:59:14 +0530

    expiry dates fixed

--- frontend/src/components/IOCharts/OpenInterest/OpenInterest.tsx
+++ frontend/src/components/IOCharts/OpenInterest/OpenInterest.tsx
@@ -6,85 +6,89 @@ import { useOutletContext } from "react-router-dom";
 type Props = {};
 
 const OpenInterest = (props: Props) => {
-  const context = useContext(ctx);
-  const outLetContext: {
-    search_context: [
-      string,
-      React.Dispatch<React.SetStateAction<string | null>>
-    ];
-    expiry_context: [string, React.Dispatch<React.SetStateAction<string>>];
-  } = useOutletContext();
-  const [searchValue, setSearchValue] = outLetContext.search_context;
-  const [ExpiryValue, setExpiryValue] = outLetContext.expiry_context;
-  const [xaxis, setXaxis] = React.useState<string[]>([
-    "2021-01-01",
-    "2021-01-02",
-    "2021-01-03",
-    "2021-01-04",
-    "2021-01-05",
-    "2021-01-06",
-  ]);
-  const [series1, setSeries1] = React.useState<number[]>([
-    30, 40, 45, 50, 49, 60,
-  ]);
-  const [series2, setSeries2] = React.useState<number[]>([
-    60, 50, 55, 40, 41, 30,
-  ]);
+	const context = useContext(ctx);
+	const outLetContext: {
+		search_context: [
+			string,
+			React.Dispatch<React.SetStateAction<string | null>>
+		];
+		expiry_context: [string, React.Dispatch<React.SetStateAction<string>>];
+	} = useOutletContext();
+	const [searchValue, setSearchValue] = outLetContext.search_context;
+	const [ExpiryValue, setExpiryValue] = outLetContext.expiry_context;
+	const [xaxis, setXaxis] = React.useState<string[]>([
+		"2021-01-01",
+		"2021-01-02",
+		"2021-01-03",
+		"2021-01-04",
+		"2021-01-05",
+		"2021-01-06",
+	]);
+	const [series1, setSeries1] = React.useState<number[]>([
+		30, 40, 45, 50, 49, 60,
+	]);
+	const [series2, setSeries2] = React.useState<number[]>([
+		60, 50, 55, 40, 41, 30,
+	]);
 
-  useEffect(() => {
-    const data = getOptionData(context);
-    setXaxis(data[0].map((strikePrice) => strikePrice.toString()));
-    setSeries1(data[1]);
-    setSeries2(data[2]);
-    console.log(searchValue, ExpiryValue);
-  }, [context, searchValue, ExpiryValue]);
+	useEffect(() => {
+		const data = getOptionData(context);
+		setXaxis(data[0].map((strikePrice) => strikePrice.toString()));
+		setSeries1(data[1]);
+		setSeries2(data[2]);
+		console.log(searchValue, ExpiryValue);
+	}, [context, searchValue, ExpiryValue]);
 
-  function getOptionData(data: Data): [string[], number[], number[]] {
-    const optionStrikePrices: string[] = [];
-    const optionCallLastOi: number[] = [];
-    const optionPutLastOi: number[] = [];
+	function getOptionData(data: Data): [string[], number[], number[]] {
+		const optionStrikePrices: string[] = [];
+		const optionCallLastOi: number[] = [];
+		const optionPutLastOi: number[] = [];
 
-    if (data && Array.isArray(data)) {
-      data.forEach((company: Company) => {
-        if (company.options && Array.isArray(company.options)) {
-          if (searchValue) {
-            if (!company.name.includes(searchValue)) return;
-          }
-          company.options?.forEach((option: Option) => {
-            const callLastIndex = option?.call?.length - 1;
-            const putLastIndex = option?.put?.length - 1;
-            if (ExpiryValue) {
-              if (!option.id.includes(ExpiryValue)) return;
-            }
-            optionStrikePrices.push(option?.id);
-            optionCallLastOi.push(option?.call[callLastIndex]?.oi || 0);
-            optionPutLastOi.push(option?.put[putLastIndex]?.oi || 0);
-          });
-        }
-      });
-    }
+		if (data && Array.isArray(data)) {
+			data.forEach((company: Company) => {
+				if (company.options && Array.isArray(company.options)) {
+					if (searchValue) {
+						if (!company.name.includes(searchValue)) return;
+					}
+					company.options?.forEach((option: Option) => {
+						const callLastIndex = option?.call?.length - 1;
+						const putLastIndex = option?.put?.length - 1;
+						if (ExpiryValue) {
+							if (!option.id.includes(ExpiryValue)) return;
+						}
+						optionStrikePrices.push(option?.id);
+						optionCallLastOi.push(
+							option?.call[callLastIndex]?.oi || 0
+						);
+						optionPutLastOi.push(
+							option?.put[putLastIndex]?.oi || 0
+						);
+					});
+				}
+			});
+		}
 
-    return [optionStrikePrices, optionCallLastOi, optionPutLastOi];
-  }
-  if (ExpiryValue && searchValue)
-    return (
-      <AreaChart
-        xaxis={xaxis}
-        series={[
-          {
-            name: "series-1",
-            data: series1,
-          },
-          {
-            name: "series-2",
-            data: series2,
-          },
-        ]}
-        width={"100%"}
-        height={300}
-      />
-    );
-  return <></>;
+		return [optionStrikePrices, optionCallLastOi, optionPutLastOi];
+	}
+	if (ExpiryValue && searchValue)
+		return (
+			<AreaChart
+				xaxis={xaxis}
+				series={[
+					{
+						name: "call",
+						data: series1,
+					},
+					{
+						name: "put",
+						data: series2,
+					},
+				]}
+				width={"100%"}
+				height={300}
+			/>
+		);
+	return <></>;
 };
 
 export default OpenInterest;
--- frontend/src/screens/OptionChan.tsx
+++ frontend/src/screens/OptionChan.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import {
-  Tabs,
-  Tab,
-  Typography,
-  Box,
-  TextField,
-  Autocomplete,
+	Tabs,
+	Tab,
+	Typography,
+	Box,
+	TextField,
+	Autocomplete,
 } from "@mui/material";
 import { Outlet, useNavigate } from "react-router";
 
@@ -13,102 +13,120 @@ import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import ctx from "../lib/Context";
 
 const options = ["ALLBANKS", "FINANCIALS", "MAINIDX", "MIDCAP"];
 
 function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
+	return {
+		id: `simple-tab-${index}`,
+		"aria-controls": `simple-tabpanel-${index}`,
+	};
 }
 
 export default function OptionChain() {
-  const [value, setValue] = React.useState(0);
-  const [expiry, setExpiry] = React.useState("");
-  const [searchValue, setSearchValue] = React.useState<string | null>(null);
-  const navigate = useNavigate();
+	const companies = useContext(ctx);
 
-  const [enabledDates, setEnabledDates] = React.useState<Dayjs[]>([
-    dayjs("2023-07-05"),
-    dayjs("2023-07-06"),
-    dayjs("2023-07-07"),
-    dayjs("2023-07-08"),
-  ]);
+	const [value, setValue] = React.useState(0);
+	const [expiry, setExpiry] = React.useState("");
+	const [searchValue, setSearchValue] = React.useState<string | null>(null);
 
-  const [date, setDate] = React.useState<Dayjs | null>(null);
+	const navigate = useNavigate();
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+	const [enabledDates, setEnabledDates] = React.useState<Dayjs[]>([
+		//   dayjs("2023-07-05"),
+		//   dayjs("2023-07-06"),
+		//   dayjs("2023-07-07"),
+		//   dayjs("2023-07-08"),
+	]);
 
-  useEffect(() => {
-    if (value === 0) {
-      navigate("opt_table");
-    } else {
-      navigate("io_chart");
-    }
-  }, [value]);
+	const [date, setDate] = React.useState<Dayjs | null>(null);
 
-  function isDateEnabled(date: Dayjs) {
-    return enabledDates.some((enabledDate) => date.isSame(enabledDate, "day"));
-  }
+	useEffect(() => {
+		if (!companies) return;
+		if (!searchValue) return;
 
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Typography variant="h4">OPTION CHAIN</Typography>
-        <hr />
-        <div
-          className="miniForm"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: 10,
-            marginBottom: 2,
-          }}
-        >
-          <Autocomplete
-            fullWidth
-            id="search-input"
-            options={options}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Search Stock/Index"
-                variant="outlined"
-              />
-            )}
-            value={searchValue}
-            onChange={(event, newValue) => setSearchValue(newValue)}
-          />
+		const company = companies.find((c) => c.name == searchValue);
+		if (!company) return;
+		const enabledDates = company.options.map((o) => dayjs(o.expiry_date));
+		setEnabledDates(enabledDates);
+	}, [companies, searchValue]);
 
-          <Box
-            sx={{
-              width: "100%",
-            }}
-          >
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker
-                label="Expiry"
-                value={date}
-                shouldDisableDate={(date) => !isDateEnabled(date)}
-                onChange={(newValue) => {
-                  setDate(newValue);
+	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
 
-                  if (newValue === null) return;
+	useEffect(() => {
+		if (value === 0) {
+			navigate("opt_table");
+		} else {
+			navigate("io_chart");
+		}
+	}, [value]);
 
-                  const formattedDate = newValue
-                    .format("DDMMMYY")
-                    .toUpperCase();
-                  setExpiry(formattedDate);
-                }}
-                sx={{ width: "100%" }}
-              />
-            </LocalizationProvider>
-          </Box>
+	function isDateEnabled(date: Dayjs) {
+		return enabledDates.some((enabledDate) =>
+			date.isSame(enabledDate, "day")
+		);
+	}
 
-          {/* <FormControl fullWidth>
+	return (
+		<Box sx={{ width: "100%" }}>
+			<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+				<Typography variant="h4">OPTION CHAIN</Typography>
+				<hr />
+				<div
+					className="miniForm"
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						marginTop: 10,
+						marginBottom: 2,
+					}}
+				>
+					<Autocomplete
+						fullWidth
+						id="search-input"
+						options={options}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								label="Search Stock/Index"
+								variant="outlined"
+							/>
+						)}
+						value={searchValue}
+						onChange={(event, newValue) => setSearchValue(newValue)}
+					/>
+
+					<Box
+						sx={{
+							width: "100%",
+						}}
+					>
+						<LocalizationProvider dateAdapter={AdapterDayjs}>
+							<DatePicker
+								label="Expiry"
+								value={date}
+								shouldDisableDate={(date) =>
+									!isDateEnabled(date)
+								}
+								onChange={(newValue) => {
+									setDate(newValue);
+
+									if (newValue === null) return;
+
+									const formattedDate = newValue
+										.format("DDMMMYY")
+										.toUpperCase();
+									setExpiry(formattedDate);
+								}}
+								sx={{ width: "100%" }}
+							/>
+						</LocalizationProvider>
+					</Box>
+
+					{/* <FormControl fullWidth>
             <InputLabel id="expirydate"> Expiry</InputLabel>
             <Select
               labelId="expiryDate"
@@ -126,28 +144,28 @@ export default function OptionChain() {
               <MenuItem value={"28SEP23"}>28 Sept, 2023</MenuItem>
             </Select>
           </FormControl> */}
-        </div>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mx: "auto",
-            my: 2,
-          }}
-        >
-          <Tab label="Option Chain" {...a11yProps(0)} />
-          <Tab label="IO Charts" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <Outlet
-        context={{
-          search_context: [searchValue, setSearchValue],
-          expiry_context: [expiry, setExpiry],
-        }}
-      />
-    </Box>
-  );
+				</div>
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					aria-label="basic tabs example"
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						mx: "auto",
+						my: 2,
+					}}
+				>
+					<Tab label="Option Chain" {...a11yProps(0)} />
+					<Tab label="IO Charts" {...a11yProps(1)} />
+				</Tabs>
+			</Box>
+			<Outlet
+				context={{
+					search_context: [searchValue, setSearchValue],
+					expiry_context: [expiry, setExpiry],
+				}}
+			/>
+		</Box>
+	);
 }
